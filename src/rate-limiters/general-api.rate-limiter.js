358d980c8794fa62b9814add8941a683adcd5c74
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-03-10 23:54:57 +0530

    chore: update rate limiters

--- src/rate-limiters/general-api.rate-limiter.js
+++ src/rate-limiters/general-api.rate-limiter.js
@@ -6,12 +6,26 @@ const { perUserAndDevice } = require("@configs/rate-limit.config");
 const welcomeAdminRateLimiter = createRateLimiter(perUserAndDevice.welcomeAdmin);
 const welcomeClientRateLimiter = createRateLimiter(perUserAndDevice.welcomeClient);
 
-const createProjectRateLimiter = createRateLimiter(perUserAndDevice.createProject);
-const updateProjectRateLimiter = createRateLimiter(perUserAndDevice.updateProject);
+const createProjectRateLimiter   = createRateLimiter(perUserAndDevice.createProject);
+const updateProjectRateLimiter   = createRateLimiter(perUserAndDevice.updateProject);
+const abortProjectRateLimiter    = createRateLimiter(perUserAndDevice.abortProject);
+const completeProjectRateLimiter = createRateLimiter(perUserAndDevice.completeProject);
+const resumeProjectRateLimiter   = createRateLimiter(perUserAndDevice.resumeProject);
+const deleteProjectRateLimiter   = createRateLimiter(perUserAndDevice.deleteProject);
+const archiveProjectRateLimiter  = createRateLimiter(perUserAndDevice.archiveProject);
+const getProjectRateLimiter      = createRateLimiter(perUserAndDevice.getProject);
+const getProjectsRateLimiter     = createRateLimiter(perUserAndDevice.getProjects);
 
 module.exports = {
     welcomeAdminRateLimiter,
     welcomeClientRateLimiter,
     createProjectRateLimiter,
     updateProjectRateLimiter,
+    abortProjectRateLimiter,
+    completeProjectRateLimiter,
+    resumeProjectRateLimiter,
+    deleteProjectRateLimiter,
+    archiveProjectRateLimiter,
+    getProjectRateLimiter,
+    getProjectsRateLimiter,
 }
