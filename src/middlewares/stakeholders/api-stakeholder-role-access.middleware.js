e637c11fa4c533e391ee0befd583f49f21f7affd
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-03-30 09:20:36 +0530

    feat(middlewares/stakeholders): update api-stakeholder-role-access middleware
    - Support for 6 phases in stakeholder role access control

--- src/middlewares/stakeholders/api-stakeholder-role-access.middleware.js
+++ src/middlewares/stakeholders/api-stakeholder-role-access.middleware.js
@@ -11,6 +11,22 @@ const stakeholderRoleAccessMiddlewares = {
     freezeElicitationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
     createMeetingStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
     cancelMeetingStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    createNegotiationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    updateNegotiationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    deleteNegotiationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    freezeNegotiationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    createSpecificationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    updateSpecificationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    deleteSpecificationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    freezeSpecificationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    createElaborationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    updateElaborationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    deleteElaborationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    freezeElaborationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    createValidationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    updateValidationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    deleteValidationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
+    freezeValidationStakeholderRoleAccessMiddleware: checkUserRoleFactory([ProjectRoleTypes.OWNER]),
 };
 module.exports = {
     stakeholderRoleAccessMiddlewares
